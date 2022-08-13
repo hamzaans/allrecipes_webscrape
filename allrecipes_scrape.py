e87ef29ae8b1ba7d4b004e920043757860831a89
hamzaans <hamza.ansari123@gmail.com>
2022-08-13 15:09:39 -0400

    Added user input

--- allrecipes_scrape.py
+++ allrecipes_scrape.py
@@ -3,25 +3,30 @@ from bs4 import BeautifulSoup
 
 # This will make user input into a search on allrecipes.com
 
+dish = input("what dish would you like to see ingredients for? ")
+dish = dish.replace(" ", "+")
 
 
 # This will get us the list of links that we can iterate through
 
-html1 = urlopen('https://www.allrecipes.com/search/results/?search=carbonara')
+html1 = urlopen('https://www.allrecipes.com/search/results/?search={}'.format(dish))
 bs = BeautifulSoup(html1.read(), 'html.parser')
 
 nameList = bs.findAll('a', {'class':"card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"})
 for name in nameList:
-    print(name.attrs.get('href'))
+    recipe_links = [(name.attrs.get('href'))]
+    print(recipe_links)
 
-# This will take the 
+# This will take the urls generated and iterate through them for scraping
 
 
 # This portion of the code scrapes one recipe and gets the list of ingredients
+for x in recipe_links:
+    html2 = urlopen(x)
+    bs = BeautifulSoup(html2.read(), 'html.parser')
 
-html2 = urlopen('https://www.allrecipes.com/recipe/282904/chicken-carbonara/')
-bs = BeautifulSoup(html2.read(), 'html.parser')
-
-nameList = bs.findAll('input', {'class':"checkbox-list-input"})
-for name in nameList:
-    print(name.attrs.get('data-ingredient'))
+    nameList = bs.findAll('input', {'class':"checkbox-list-input"})
+    for name in nameList:
+        if name == None:
+            continue
+        print(name.attrs.get('data-ingredient'))
